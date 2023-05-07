96019604c83d34ec7ee7ce32c714518498c5f0f7
Uthaeus <romanlavery@gmail.com>
2023-05-07 11:21:07 -0600

    successfully creating new meetups

--- src/auth/logout.js
+++ src/auth/logout.js
@@ -4,27 +4,27 @@ import { useNavigate } from "react-router";
 function Logout() {
     const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch("http://localhost:4000/logout", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
+    // useEffect(() => {
+    //     fetch("http://localhost:4000/logout", {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Content-Type": "application/json",
                 
-            }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Logout failed");
-            }
-            //navigate("/");
-            return response.json();
-        })
-        .then(data => {
-            localStorage.removeItem("token");
-            navigate("/");
-        })
-        .catch(error => console.log('logout error: ', error));
-    }, [navigate]);
+    //         }
+    //     })
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error("Logout failed");
+    //         }
+    //         //navigate("/");
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         localStorage.removeItem("token");
+    //         navigate("/");
+    //     })
+    //     .catch(error => console.log('logout error: ', error));
+    // }, [navigate]);
 }
 
 export default Logout;
--- src/meetups/meetup-form.js
+++ src/meetups/meetup-form.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
 function MeetupForm({ meetup = {}}) {
+    const navigate = useNavigate();
     const { register, handleSubmit, errors, reset } = useForm({
         defaultValues: {
             title: meetup.title || "",
@@ -14,49 +16,78 @@ function MeetupForm({ meetup = {}}) {
         }
     });
 
+    function buildForm(data) {
+        const formData = new FormData();
 
-    function onSubmit(data) {
+        formData.append("meetup[title]", data.title);
+        formData.append("meetup[location]", data.location);
+        formData.append("meetup[time]", data.time);
+        formData.append("meetup[date]", data.date);
+        formData.append("meetup[description]", data.description);
+        formData.append("meetup[main_image]", data.main_image[0]);
+        formData.append("meetup[thumbnail_image]", data.thumbnail_image[0]);
+
+        return formData;
+    }
+
+
+    function submitHandler(data) {
         console.log(data);
+
+
+        fetch("http://localhost:4000/meetups", {
+            method: "POST",
+            body: buildForm(data)
+        })
+            .then((response) => {
+                if (response.ok) {
+                    navigate("/meetups");
+                    return response.json();
+                }
+            })
+            .catch((error) => {
+                console.log("meetup create error", error);
+            });
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(submitHandler)}>
             <div className="form-group mb-2">
                 <label htmlFor="title">Title</label>
-                <input className="form-control" type="text" name="title" id="title" {...register({ required: true })} />
-                {errors.title && <span>Title is required</span>}
+                <input className="form-control" type="text" name="title" id="title" {...register("title", { required: true })} />
+                {errors?.title && <span>Title is required</span>}
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="location">Location</label>
-                <input className="form-control" type="text" name="location" id="location" {...register({ required: true })} />
-                {errors.location && <span>Location is required</span>}
+                <input className="form-control" type="text" name="location" id="location" {...register("location", { required: true })} />
+                {errors?.location && <span>Location is required</span>}
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="time">Time</label>
-                <input className="form-control" type="time" name="time" id="time" {...register({ required: true })} />
+                <input className="form-control" type="time" name="time" id="time" {...register("time", { required: true })} />
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="date">Date</label>
-                <input className="form-control" type="date" name="date" id="date" {...register({ required: true })} />
+                <input className="form-control" type="date" name="date" id="date" {...register("date", { required: true })} />
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="main_image">Main Image</label>
-                <input className="form-control" type="file" name="main_image" id="main_image" {...register({ required: true })} />
+                <input className="form-control" type="file" name="main_image" id="main_image" {...register("main_image")} />
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="thumbnail_image">Thumbnail Image</label>
-                <input className="form-control" type="file" name="thumbnail_image" id="thumbnail_image" {...register({ required: true })} />
+                <input className="form-control" type="file" name="thumbnail_image" id="thumbnail_image" {...register("thumbnail_image")} />
             </div>
 
             <div className="form-group mb-2">
                 <label htmlFor="description">Description</label>
-                <textarea className="form-control" name="description" id="description" {...register({ required: true })} />
-                {errors.description && <span>Description is required</span>}
+                <textarea className="form-control" name="description" id="description" {...register("description", { required: true })} />
+                {errors?.description && <span>Description is required</span>}
             </div>
 
             <div className="form-group">
