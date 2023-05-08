5a4562965c8f096d5b0b45138477c7f6cd06b58a
Uthaeus <romanlavery@gmail.com>
2023-05-08 17:01:47 -0600

    added styling to meetup detail component

--- src/meetups/meetup-detail.js
+++ src/meetups/meetup-detail.js
@@ -5,119 +5,145 @@ import { Link } from "react-router-dom";
 import Image from "../ui/Image";
 
 function MeetupDetail() {
-    const [meetup, setMeetup] = useState({});
-    const [comments, setComments] = useState([]);
-    const [comment, setComment] = useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const { meetupId } = useParams();
-    const navigate = useNavigate();
+  const [meetup, setMeetup] = useState({});
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { meetupId } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (localStorage.getItem("token")) {
-            setIsLoggedIn(true);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    }
+
+    fetch(`http://localhost:4000/meetups/${meetupId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setMeetup(data);
+        setComments(data.comments);
+      })
+      .catch((error) => {
+        console.log("meetup show error", error);
+      });
+  }, [meetupId]);
+
+  function commentChangeHandler(event) {
+    setComment(event.target.value);
+  }
+
+  function commentSubmitHandler(e) {
+    e.preventDefault();
+
+    fetch("http://localhost:4000/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token: localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        comment: {
+          content: comment,
+          meetup_id: meetupId,
+        },
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         }
+      })
+      .then((data) => {
+        setComments([data, ...comments]);
+        setComment("");
+      })
+      .catch((error) => {
+        console.log("comment create error", error);
+      });
+  }
 
-        fetch(`http://localhost:4000/meetups/${meetupId}`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setMeetup(data);
-                setComments(data.comments);
-            })
-            .catch((error) => {
-                console.log('meetup show error', error);
-            });
-    }, [meetupId]);
+  function deleteHandler() {
+    fetch(`http://localhost:4000/meetups/${meetupId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          navigate("/meetups");
+          return response.json();
+        }
+      })
+      .catch((error) => {
+        console.log("meetup delete error", error);
+      });
+  }
 
-    function commentChangeHandler(event) {
-        setComment(event.target.value);
-    }
+  let meetupUser = meetup.user ? meetup.user.username : "anonymous";
+  let imageUrl = meetup.main_image ? `http://localhost:4000${meetup.main_image.url}` : "https://via.placeholder.com/600x300";
 
-    function commentSubmitHandler(e) {
-        e.preventDefault();
+  return (
+    <div className="meetup-detail-container">
+      <div className="detail-col-left">
 
-        fetch("http://localhost:4000/comments", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "token": localStorage.getItem("token")
-            },
-            body: JSON.stringify({
-                comment: {
-                    content: comment,
-                    meetup_id: meetupId
-                }
-            })
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                setComments([data, ...comments]);
-                setComment("");
-            })
-            .catch((error) => {
-                console.log("comment create error", error);
-            });
-    }
-
-    function deleteHandler() {
-        fetch(`http://localhost:4000/meetups/${meetupId}`, {
-            method: "DELETE"
-        })
-            .then((response) => {
-                if (response.ok) {
-                    navigate("/meetups");
-                    return response.json();
-                }
-            })
-            .catch((error) => {
-                console.log("meetup delete error", error);
-            });
-    }
-
-    return (
-        <div>
-            <div>
-                <div>
-                    <h1>{meetup.title}</h1>
-                    {meetup.id}
-                    <p>{meetup.description}</p>
-                    <p>User info: {meetup.user_id} | {meetup.user?.username}</p>
-                </div> 
-
-                <div>
-                    <Image src={meetup.main_image?.url} alt={meetup.title} type='main' />
-                </div>
-
-                <div>
-                    {isLoggedIn === true && (
-                        <div>
-                            <textarea placeholder="Add a comment" onChange={commentChangeHandler} value={comment} rows={5} />
-                            <button onClick={commentSubmitHandler}>Submit</button>
-                        </div>
-                    )}
-                    {comments?.map((comment) => {
-                        return (
-                            <div key={comment.id}>
-                                <p>{comment.content}</p>
-                                <p>User info: {comment.user_id} | {comment.user?.username}</p>
-                            </div>
-                        );
-                    })}
-                </div>
+        <div className="detail-header">
+            <div className="detail-title-wrapper">
+                <h1 className="detail-title">{meetup.title}</h1>
+                <p className="detail-user">posted by {meetupUser}</p>
             </div>
 
-            <div>
-                <button onClick={deleteHandler}>Delete</button>
-                <Link to={`/meetups/${meetupId}/edit`}>Edit</Link>
-                <Link to="/meetups">Back to Meetups</Link>
+            <div className="details-wrapper">
+                <p className="detail-item">{meetup.location}</p>
+                <p className="detail-item">{meetup.date}</p>
+                <p className="detail-item">{meetup.time}</p>
             </div>
         </div>
-    );
+
+        <div className="detail-image" style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "300px",
+        }} />
+
+        <p className="detail-description">{meetup.description}</p>
+
+        <div className="detail-actions">
+          <button onClick={deleteHandler}>Delete</button>
+          <Link to={`/meetups/${meetupId}/edit`}>Edit</Link>
+          <Link to="/meetups">Back to Meetups</Link>
+        </div>
+      </div>
+
+      <div className="detail-col-right">
+        <div>
+          {isLoggedIn === true && (
+            <div>
+              <textarea
+                placeholder="Add a comment"
+                onChange={commentChangeHandler}
+                value={comment}
+                rows={5}
+              />
+              <button onClick={commentSubmitHandler}>Submit</button>
+            </div>
+          )}
+          {comments?.map((comment) => {
+            return (
+              <div key={comment.id}>
+                <p>{comment.content}</p>
+                <p>
+                  User info: {comment.user_id} | {comment.user?.username}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MeetupDetail;
